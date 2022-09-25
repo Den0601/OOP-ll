eb59ff2e9d70440c1ac17e7bdc602427d7fe89b3
黃芊蝀 <61945850+Den0601@users.noreply.github.com>
2022-09-26 06:07:38 +0800

    Add files via upload

---
+++ w2 question1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class homework1 {
+    public static void main(String[] args) {
+        var num = new TreeSet<>();
+        var num1 = new TreeSet<>();
+        for (int i = 1; i <= 10; i++) {
+            double a = Math.floor(Math.random() * 100 + 1);
+            if (a >= 30 && a <= 70) {
+                num1.add(a);
+            }
+            num.add(a);
+            System.out.printf("第% d 個號碼:%f\n", i, a);
+        }
+        System.out.printf("物件內元素個數為: %d\n", num.size());
+        System.out.printf("物件內元素的內容:");
+        System.out.println(num);
+        System.out.printf("第一個元素內容為:");
+        System.out.println(num.first());
+        System.out.printf("最後一個元素內容:");
+        System.out.println(num.last());
+        System.out.printf("內容介於30~70者:");
+        System.out.println(num1);
+    }
+
+}
---
+++ w2 question2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class homework2 {
+    public static void main(String[] args) {
+        var month = new HashMap<>();
+        System.out.println("請輸入1~12");
+        Scanner input = new Scanner(System.in);
+        String i = input.nextLine();
+        month.put("1", "January");
+        month.put("2", "February");
+        month.put("3", "March");
+        month.put("4", "April");
+        month.put("5", "May");
+        month.put("6", "June");
+        month.put("7", "July");
+        month.put("8", "Augest");
+        month.put("9", "September");
+        month.put("10", "October");
+        month.put("11", "November");
+        month.put("12", "December");
+        while (month.containsKey(i) == false) {
+
+            System.out.println("範圍錯誤");
+            System.out.println("請輸入1~12");
+
+            String e = input.nextLine();
+            if (month.containsKey(i) == true || month.containsKey(e) == true) {
+                i = e;
+                break;
+            }
+        }
+        if (month.containsKey(i) == true) {
+            System.out.printf("第%s月的英文單字為 ", i);
+            System.out.println(month.get(i));
+        }
+        input.close();
+
+    }
+}
